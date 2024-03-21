36c6835188576faab323be9ef380650c415d3b8f
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-03-21 14:05:27 +0300

    not finished

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const returnFirstTwoDrivers = function(drivers){
+    drivers= ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return (drivers.slice(0,2))
+}
+
+
+
+/*const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers){
+drivers = ['Antonia', 'Nuru'];
+}
+function returnLastTwoDrivers(){
+    drivers = ['Amari', 'Mo'];
+}*?
+
+
+
 // Code your solution in this file!
